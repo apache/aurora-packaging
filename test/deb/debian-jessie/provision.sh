0614c78cd94baf32b6eaa274315b38eb0d075f81
Stephan Erb <serb@apache.org>
2016-06-29 00:01:33 +0200

    Update packaging for Aurora 0.14
    Reviewed at https://reviews.apache.org/r/49338/

--- test/deb/debian-jessie/provision.sh
+++ test/deb/debian-jessie/provision.sh
@@ -6,8 +6,8 @@ apt-get install -y openjdk-8-jre libsvn1 zookeeperd
 
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 
-package=mesos_0.26.0-0.2.145.debian81_amd64.deb
-wget -c https://downloads.mesosphere.io/master/debian/8/$package
+package=mesos_0.27.2-2.0.15.debian81_amd64.deb
+wget -c http://repos.mesosphere.com/debian/pool/main/m/mesos/$package
 dpkg -i $package
 
 # NOTE: This appears to be a missing dependency of the mesos deb package and is needed
--- test/deb/ubuntu-trusty/provision.sh
+++ test/deb/ubuntu-trusty/provision.sh
@@ -6,8 +6,8 @@ apt-get install -y openjdk-8-jre libsvn1 zookeeperd
 
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 
-package=mesos_0.26.0-0.2.145.ubuntu1404_amd64.deb
-wget -c https://downloads.mesosphere.io/master/ubuntu/14.04/$package
+package=mesos_0.27.2-2.0.15.ubuntu1404_amd64.deb
+wget -c http://repos.mesosphere.com/ubuntu/pool/main/m/mesos/$package
 dpkg -i $package
 
 # NOTE: This appears to be a missing dependency of the mesos deb package and is needed
