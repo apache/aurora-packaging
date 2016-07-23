2a9dd402b7b0fe75d994ed83de36f820619fd836
Maxim Khutornenko <maxim@apache.org>
2016-07-23 18:40:56 +0200

    Update package scripts to 0.15.0.
    Reviewed at https://reviews.apache.org/r/49732/

--- test/rpm/centos-7/provision.sh
+++ test/rpm/centos-7/provision.sh
@@ -6,7 +6,7 @@ service zookeeper-server init
 systemctl start zookeeper-server
 
 rpm -Uvh https://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
-yum -y install mesos-0.27.2
+yum -y install mesos-0.28.2
 
 systemctl start mesos-master
 systemctl start mesos-slave
